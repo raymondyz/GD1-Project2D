cbbc8baaef2383607cc5ebe4cae537b0460488dd
Raymond Zou <raymond.yh.zou@gmail.com>
2026-01-15 16:11:06 -0800

    Movement

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    float movementX;
+    float movementY;
+    [SerializeField] float speed = 5.0f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float movementDistanceX = movementX * speed * Time.deltaTime;
+        float movementDistanceY = movementY * speed * Time.deltaTime;
+
+        transform.position = new Vector2(transform.position.x + movementDistanceX, transform.position.y + movementDistanceY);
+
+    }
+
+    void OnMove(InputValue value)
+    {
+        Vector2 v = value.Get<Vector2>();
+
+        movementX = v.x;
+        movementY = v.y;
+
+        Debug.Log(movementX);
+        Debug.Log(movementY);
+    }
+}
